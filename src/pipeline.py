b700753a529ecefcc2b908acf1826541aaddcec1
Francesc Font Clos <francesc.font@gmail.com>
2017-07-22 23:16:40 +0200

    finished pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from cleanup import strip_headers
 from tokenizer import tokenize_text
+from collections import Counter
 import io
 import os
 
@@ -29,8 +30,20 @@ def process_book(
     with io.open(target_file,"w") as f:
         f.write(clean)
 
+    # compute tokens
     tokens = tokenize_f(clean)
 
     # write tokens file
-    # TODO
+    target_file = os.path.join(tokens_dir,"PG%s_tokens.txt"%PG_number)
+    with io.open(target_file,"w") as f:
+        f.write("\n".join(tokens))
 
+    # compute counts
+    counts = Counter(tokens)
+
+    # write counts file
+    target_file = os.path.join(counts_dir,"PG%s_counts.txt"%PG_number)
+    with io.open(target_file,"w") as f:
+        f.write("\n".join([w+"\t"+str(c) for w,c in counts.most_common()]))
+    
+    
