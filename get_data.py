5792572392534415a2f712acedc63af7da1a7e20
Francesc Font Clos <francesc.font@gmail.com>
2018-08-12 21:28:20 +0200

    parse dataframes

--- get_data.py
+++ get_data.py
@@ -10,6 +10,10 @@ from src.metadataparser import make_df_metadata
 
 import argparse
 import os
+import glob
+import numpy as np
+import pandas as pd
+import lxml.html
 import subprocess
 
 if __name__ == '__main__':
@@ -124,34 +128,83 @@ if __name__ == '__main__':
         quiet=args.quiet
         )
 
-    # Update metadata
-    # ---------------
-    # By default, update the whole metadata csv
-    # file each time new data is downloaded.
-    make_df_metadata(
-        path_xml=os.path.join(args.metadata, 'rdf-files.tar.bz2'),
-        path_out=os.path.join(args.metadata, 'metadata.csv'),
-        update=args.keep_rdf
-        )
+    if False:
+        # Update metadata
+        # ---------------
+        # By default, update the whole metadata csv
+        # file each time new data is downloaded.
+        make_df_metadata(
+            path_xml=os.path.join(args.metadata, 'rdf-files.tar.bz2'),
+            path_out=os.path.join(args.metadata, 'metadata.csv'),
+            update=args.keep_rdf
+            )
 
-    # Create bookshelves
-    # ------------------
-    # Parse the bookshelves data from www.gutenberg.org/wiki/
+        # Create bookshelves
+        # ------------------
+        # Scrap the bookshelves data from www.gutenberg.org/wiki/
+        sp_args = ["wget",
+                   "--random-wait", "-r",
+                   "-A", "*Bookshelf*",
+                   "-p", "--no-parent",
+                   "-e", "robots=off",
+                   "-U", "mozilla",
+                   "http://www.gutenberg.org/wiki/Category:Bookshelf"
+                   ]
+        subprocess.call(sp_args)
+
+        # move it to metadata dir
+        sp_args = "mv www.gutenberg.org/wiki/*Bookshelf* metadata/bookshelves/"
+        subprocess.call(sp_args, shell=True)
+
+        # cleanup
+        sp_args = ["rm", "-rf", "www.gutenberg.org"]
+        subprocess.call(sp_args)
+
     # parse the data
-    sp_args = ["wget",
-               "--random-wait", "-r",
-               "-A", "*Bookshelf*",
-               "-p", "--no-parent",
-               "-e", "robots=off",
-               "-U", "mozilla",
-               "http://www.gutenberg.org/wiki/Category:Bookshelf"
-               ]
-    subprocess.call(sp_args)
+    BS_paths = glob.glob("metadata/bookshelves/*Bookshelf*")
+    BS = [path.split("/")[-1] for path in BS_paths]
 
-    # move it to metadata dir
-    sp_args = "mv www.gutenberg.org/wiki/*Bookshelf* metadata/bookshelves/"
-    subprocess.call(sp_args, shell=True)
+    BS_dict = {}
+    for path in BS_paths:
+        bs = path.split("/")[-1]
+        BS_dict[bs] = []
+        with open(path, "r") as foo:
+            dom = lxml.html.fromstring(foo.read())
+            # select the url in href for all a tags(links)
+            for link in dom.xpath('//a/@href'):
+                # links to ebooks that are not searches
+                if link.find("ebooks") > -1 and link.find("search") == -1:
+                    PGid = "PG"+link.split("/")[-1]
+                    BS_dict[bs].append(PGid)
 
-    # cleanup
-    sp_args = ["rm", "-rf", "www.gutenberg.org"]
-    subprocess.call(sp_args)
+        # delete empty BSs
+        if len(BS_dict[bs]) == 0:
+            del BS_dict[bs]
+
+    # recompose list of BSs
+    BS = list(BS_dict.keys())
+
+    # list of unique PGids
+    PGids = list(set(np.concatenate(list(BS_dict.values()))))
+    # put in a DataFrame
+    df = pd.DataFrame(index=PGids, columns=BS)
+    for k, v in BS_dict.items():
+        df.loc[v, k] = True
+    df.to_pickle("metadata/bookshelves_raw.p")
+
+    # filter by size
+    min_books = 50
+    max_books = 150
+    sdf = df.loc[:, (df.sum() > min_books) & (df.sum() <= max_books)].dropna(how="all")
+
+    # deal with overlaps
+    sdf = sdf.loc[sdf.sum(axis=1) == 1].dropna(how="all", axis=1)
+    sdf = sdf.drop("Esperanto_(Bookshelf)", axis=1).dropna(how="all")
+
+    # filter by language
+    from src.metaquery import meta_query
+    mq = meta_query(path="metadata/metadata.csv")
+    mq.filter_lang("en", how="only")
+    allPGs = mq.df.set_index("id").index
+    sdf = sdf.loc[np.intersect1d(sdf.index, allPGs)].dropna(how="all", axis=1)
+    sdf.to_pickle("metadata/bookshelves_clean.p")
