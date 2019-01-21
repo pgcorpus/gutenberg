f3a079600ae5b6044bca6623acf2ef77c07be110
Francesc Font Clos <francesc.font@gmail.com>
2019-01-21 09:02:40 +0100

    handling correct relative part in importing nltk so that this can be used in tests

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -3,12 +3,13 @@
    Call tokenize and pass a text (i.e. as a string).
    You will get a list of tokens
 """
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize.treebank import TreebankWordTokenizer
 
 import nltk
-nltk.data.path=["src/nltk_data"]
-
-from nltk.tokenize.treebank import TreebankWordTokenizer
-from nltk.tokenize import sent_tokenize
+import os
+filepath, _ = os.path.split(__file__)
+nltk.data.path = [os.path.join(filepath, "nltk_data")]
 
 
 def tokenize_text(text, language="english"):
@@ -16,29 +17,30 @@ def tokenize_text(text, language="english"):
     Use NLTK's Treebankwordtokenizer.
     Note that we first split into sentences using NLTK's sent_tokenize.
     We additionally call a filtering function to remove un-wanted tokens.
-    
+
     IN:
     - text, str
     OUT:
     - list of strings
     '''
-    ## list of tokens
+    # list of tokens
     list_tokens = []
-    
-    ## split text into sentences
-    sentences=sent_tokenize(text, language=language)
-    
-    ## define the tokenizer
+
+    # split text into sentences
+    sentences = sent_tokenize(text, language=language)
+
+    # define the tokenizer
     tokenizer = TreebankWordTokenizer()
-    ## loop over all sentences
+    # loop over all sentences
     for sent in sentences:
-        ## tokenize the sentence
+        # tokenize the sentence
         sent_tokenized = tokenizer.tokenize(sent)
-        ## lowercase the tokens
-        ## add tokens to list of tokens
+        # lowercase the tokens
+        # add tokens to list of tokens
         list_tokens += sent_tokenized
     list_tokens = filter_tokens(list_tokens)
     return list_tokens
+
 
 def filter_tokens(list_tokens):
     '''Remove un-wanted tokens from list of tokens
